1a011a5bf1c48251dba17fa2c1cbd36838a0d43d
Jorg van der Ham <jorgvanderham@Jorgs-MacBook-Pro.local>
2020-06-30 20:28:02 +0200

    added lesson 1 example

---
+++ module1/lesson 001/example.js
@@ -0,0 +1,52 @@
+// number
+let age = 32
+const boolean = true // or false
+const nothing = null
+const anotherNothing = undefined
+
+let someVar
+
+console.log(someVar)
+
+const something = 3**3
+
+console.log(something)
+
+const modulo = 7 % 3;
+
+// age = age + 1
+age += 1
+
+age++
+
+console.log(age)
+
+
+// string
+//Kenouly
+let Kenouly = 'Love, Rosie';
+//Anna
+let Anna = "The Disaster Artist";
+// Marcella
+const marcella = 'the hobbit';
+// Yuka
+let Yuka = 'Inherent Vice';
+//Mahmut
+let mahmut = "love";
+//Lisette
+let lisetteVenema = 'the gruffalo who is a parent to other gruffalo';
+
+const classBooks = mahmut + marcella
+console.log(classBooks)
+
+console.log(Anna.length)
+
+console.log(Anna[2])
+
+console.log(Anna.charAt(2))
+
+console.log(lisetteVenema.indexOf('gruffalo', 13))
+
+console.log(lisetteVenema.substring(13, 16))
+
+console.log(lisetteVenema.substr(13, 3))
