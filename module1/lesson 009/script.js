ed6edc36d390497075c6ed10ef325d4afabbf9fd
Jorg van der Ham <jorgvanderham@Jorgs-MacBook-Pro.local>
2020-07-18 12:43:33 +0200

    added example code from lesson 9

---
+++ module1/lesson 009/script.js
@@ -0,0 +1,16 @@
+console.log('hello');
+
+let something = 12;
+
+function greeter() {
+  const hello = 'hello';
+  something++;
+  console.log(hello);
+  console.log("sdfsdf");
+}
+
+function callGreeter() {
+  greeter();
+}
+
+callGreeter();
